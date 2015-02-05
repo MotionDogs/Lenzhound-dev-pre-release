12767d73c38affc922fa818b550c37c15c66eb2e
LoneWolf367 <lonewolf367@gmail.com>
2015-02-04 18:05:04 -0800

    Revert dde5fd7..b81a508
    This rolls back to commit dde5fd7ad0cef63a5873df856d3765a9dd43312f.

---
+++ Rxr/motor.cpp
@@ -0,0 +1,167 @@
+#include "motor.h"
+#include "macros.h"
+#include "constants.h"
+#include "util.h"
+
+#define PERCENT_CONVERSION_FACTOR .01
+
+Motor::Motor() :
+microsteps_(0),
+direction_(1),
+max_velocity_(0),
+current_velocity_cap_(0),
+accel_(0),
+decel_(0),
+decel_denominator_(0),
+velocity_(0),
+calculated_position_(0),
+motor_position_(0),
+observed_position_(0),
+run_count_(0),
+sleeping_(false)
+{
+}
+
+void Motor::set_observed_position(long position) {
+  long new_position = position >> (3 - microsteps_);
+  if (new_position != observed_position_) {
+    WakeUp();
+    run_count_ = 0;
+  }
+  observed_position_ = new_position;
+}
+
+void Motor::set_max_velocity(int velocity) {
+  current_velocity_cap_ = max(max_velocity_ * velocity * PERCENT_CONVERSION_FACTOR, 1L);
+}
+
+void Motor::Configure(
+  long accel, long max_velocity, char microsteps) {
+  char microstep_difference = microsteps - microsteps_;
+  microsteps_ = microsteps;
+  accel_ = accel << microsteps_;
+  max_velocity_ = max_velocity << microsteps_;
+  decel_ = accel_;
+  decel_denominator_ = util::FixedMultiply(decel_, util::MakeFixed(2L));
+
+  // negative shifting is undefined behavior in c++
+  if (microstep_difference >= 0) {
+    observed_position_ <<= microstep_difference;
+    calculated_position_ <<= microstep_difference;
+    motor_position_ <<= microstep_difference;
+    velocity_ <<= microstep_difference;
+  } else {
+    observed_position_ >>= -microstep_difference;
+    calculated_position_ >>= -microstep_difference;
+    motor_position_ >>= -microstep_difference;
+    velocity_ >>= -microstep_difference;
+  }
+
+  switch (microsteps_) {
+    case FULL_STEPS: {
+      MS1_PIN(CLR);
+      MS2_PIN(CLR);
+      break;
+    }
+    case HALF_STEPS: {
+      MS1_PIN(SET);
+      MS2_PIN(CLR);
+      break;
+    }
+    case QUARTER_STEPS: {
+      MS1_PIN(CLR);
+      MS2_PIN(SET);
+      break;
+    }
+    case EIGHTH_STEPS: {
+      MS1_PIN(SET);
+      MS2_PIN(SET);
+      break;
+    }
+  }
+}
+
+void Motor::Pulse() {
+  STEP_PIN(SET);
+  STEP_PIN(CLR);
+}
+
+void Motor::SetDirForward() {
+  DIR_PIN(SET);
+  direction_ = 1;
+}
+
+void Motor::SetDirBackward() {
+  DIR_PIN(CLR);
+  direction_ = 0;
+}
+
+long Motor::GetDecelerationThreshold() {
+  return util::FixedDivide(
+    util::FixedMultiply(velocity_,velocity_),decel_denominator_);
+}
+
+void Motor::Sleep() {
+  sleeping_ = true;
+  run_count_ = 0;
+  SLEEP_PIN(CLR);
+}
+
+void Motor::WakeUp() {
+  sleeping_ = false;
+  SLEEP_PIN(SET);
+}
+
+bool Motor::TrySleep() {
+  if (motor_position_ != observed_position_) {
+    run_count_ = 0;
+    return false;
+  }
+  if (sleeping_) {
+    return true;
+  }
+  if (run_count_ > kSleepThreshold) {
+    Sleep();
+    return true;
+  }
+  run_count_++;
+  return false;
+}
+
+void Motor::Run() {
+  if (TrySleep()) {
+    return;
+  }
+  long steps_to_go = util::Abs(observed_position_ - calculated_position_);
+  if(direction_) {
+    if((calculated_position_ > observed_position_) ||
+      (steps_to_go <= GetDecelerationThreshold())) {
+      velocity_ -= decel_;
+    } else if (calculated_position_ < observed_position_) {
+      velocity_ = util::Min(velocity_+accel_, current_velocity_cap_);
+    }
+    calculated_position_ += velocity_;
+    if((motor_position_ < calculated_position_) &&
+      (motor_position_ != observed_position_)) {
+      motor_position_ += util::kFixedOne;
+      Pulse();
+    }
+    if(velocity_ < 0)
+      SetDirBackward();
+  } else {
+    if((calculated_position_ < observed_position_) ||
+      (steps_to_go <= GetDecelerationThreshold())){
+      velocity_ += decel_;
+    } else if (calculated_position_ > observed_position_) {
+      velocity_ = util::Max(velocity_-accel_, -current_velocity_cap_);
+    }
+    calculated_position_ += velocity_;
+    if(motor_position_ > calculated_position_ &&
+      motor_position_ != observed_position_) {
+      motor_position_ -= util::kFixedOne;
+      Pulse();
+    }
+    if(velocity_ > 0)
+      SetDirForward();
+  }
+}
--- Rxr/receiver.cpp
+++ Rxr/receiver.cpp
@@ -7,10 +7,10 @@
 #include "macros.h"
 #include "receiver.h"
 #include "util.h"
-#include "constants.h"
 
+
+#define PLAYBACK_MODE 1
 #define MAX_VELOCITY 100
-#define MAX_ACCELERATION 100
 
 #define RATE_MASK     0b00101000
 #define RATE_250KB    0b00100000
@@ -52,7 +52,7 @@ void Receiver::LoadSettings()
     reg[0] |= rates[setting];
   }
   setting = settings.GetChannel();
-  if (setting >= 1 && setting <= 82) {
+  if (setting >= 0 && setting <= 84) {
     Mirf.channel = setting;
   }
   setting = settings.GetAntenna();
@@ -85,15 +85,9 @@ long Receiver::Position(){
 }
 
 int Receiver::Velocity() {
-  if(packet_.mode==PLAYBACK_MODE || packet_.mode==Z_MODE)
+  if(packet_.mode==PLAYBACK_MODE)
     return packet_.velocity;
   return MAX_VELOCITY;
-}
-
-int Receiver::Acceleration() {
-  if(packet_.mode==Z_MODE)
-    return packet_.acceleration;
-  return MAX_ACCELERATION;
 }
 
 int Receiver::Mode(){
